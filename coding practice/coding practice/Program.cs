c73b34a045ca8dabce207d03276ecbc9b8b27dbd
Sai Krishna <saikrishnahsr0@gmail.com>
2023-06-16 17:24:35 +0530

    WordCount and FizzBuzz solution added

---
+++ coding practice/coding practice/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using coding_practice;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter the String");
+        string sentance = Console.ReadLine();
+        Dictionary<string, int> dict = new Dictionary<string, int>();
+        WordCount obj = new WordCount();
+        dict = obj.WordCountSol(sentance);
+
+        foreach (var d in dict)
+        {
+            Console.WriteLine(d.Key + " : " + d.Value);
+        }
+
+
+        FizzBuzz obj2 = new FizzBuzz();
+        Console.WriteLine("Enter the starting element");
+        int start = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the ending element");
+        int end = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the first divisor");
+        int divisor1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the second divisor");
+        int divisor2 = Convert.ToInt32(Console.ReadLine());
+        var result = obj2.FizzBuzzSol(start, end, divisor1, divisor2);
+        foreach (var item in result)
+        {
+            Console.WriteLine(item);
+        }
+    }
+
+
+}
---
+++ coding practice/coding practice/WordCount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coding_practice
+{
+    class WordCount
+    {
+        public Dictionary<string, int> WordCount(string sentance)
+        {
+            sentance = sentance.ToLower();
+            string[] words = sentance.Split(' ');
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (dict.ContainsKey(words[i]))
+                {
+                    dict[words[i]]++;
+                }
+                else
+                {
+                    dict.Add(words[i], 1);
+                }
+            }
+            return dict;
+        }
+    }
+
+}
